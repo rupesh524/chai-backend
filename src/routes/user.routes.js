fb11565a3fec9c51b5968d96a20229a594ae7e2c
Rupesh Mandliya <rupeshmandliya167@gmail.com>
2025-03-01 15:04:59 +0530

    add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,7 +9,7 @@
 // export default router
 
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, GetWatchHistory, registerUser, updateAccountDetails, UpdateCoverImage, updateUserAvatar } from "../controllers/user.controller.js";
 import { loginUser} from "../controllers/user.controller.js"
 import  {upload} from   "../middlewares/multer.middleware.js"
 import { LogoutUser } from "../controllers/user.controller.js";
@@ -42,6 +42,27 @@ router.route("/logout").post(verifyJWT, LogoutUser)
 
 // isme verify jwt ki jarurat nahi hai 
 router.route("/refresh-token").post(refreshAccessToken)
+
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);     // verify jwt means logged in user hi usko 
+                                                                        // change kar paye
+
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-account").patch(verifyJWT,updateAccountDetails); // post me sari details update ho jayegi hamko 
+                                                    // sari details update nahi karna thi 
+
+ // khali avatar update karna hai toh patch then only single file toh upload middileware me single then method call
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),UpdateCoverImage);
+
+
+// particular username ke channel ki profile nikalni hai toh ese hi likhna padega 
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+
+router.route("/history").get(verifyJWT,GetWatchHistory);
+
 
 // Export the router
 export default router;
